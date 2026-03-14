d7b8df64e316d17c1655b72c9efe769991450f3f
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-14 22:19:16 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Inside the countries component</h1>
+        </div>
+    );
+};
+
+export default Countries;
