b6ce9da974d3cc9079f58d890b67f45d9937c685
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-14 23:05:06 +0600

    Added component named Country and Countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Countries from './components/Countries/Countries'
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./components/Countries/Countries";
 
+const countriesPromise = fetch("https://openapi.programming-hero.com/api/all").then((res) => res.json());
 function App() {
-  return (
-    <>
-          <Countries></Countries>
-    </>
-  )
+    return (
+        <>
+            <Suspense fallback={<p>Countries LOADING...</p>}>
+                <Countries countriesPromise={countriesPromise}></Countries>
+            </Suspense>
+        </>
+    );
 }
 
-export default App
+export default App;
