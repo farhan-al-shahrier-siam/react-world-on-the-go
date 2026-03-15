5ef50367eceebe431cbaff6562aaa0787f9027b5
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-15 11:29:13 +0600

    Added two css file for each components and added a not visited btn which we can toggle

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./Country.css";
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        // basic toggle system
+        /* if (visited) {
+            setVisited(false);
+        } else {
+            setVisited(true);
+        } */
+
+        // ternary toggle system
+        /* setVisited(visited ? false : true); */
+
+        // third system
+        setVisited(!visited);
+    };
+
     return (
-        <div>
+        <div className={`country ${visited && "country-visited"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Capital: {country.capital.capital}</p>
             <p>Population: {country.population.population}</p>
+            <p>
+                Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}
+            </p>
+            <button onClick={handleVisited}>{visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
